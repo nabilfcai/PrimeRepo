bf0755534481331d5cea827e580073316f8cd955
AhmedEhabH <ahmed.ehab.h@gmail.com>
2023-12-23 20:45:17 +0200

    initial commit add is_prime function test is_prime

---
+++ main.py
@@ -0,0 +1,20 @@
+def is_prime(number:int) -> bool:
+    if number <= 1:
+        return False
+    elif number == 2:
+        return True
+    elif number % 2 == 0:
+        return False
+    else:
+        # Check for factors up to the square root of the number
+        for i in range(3, int(number**0.5) + 1, 2):
+            if number % i == 0:
+                return False
+        return True
+
+# Example usage:
+number_to_check = 17
+if is_prime(number_to_check):
+    print(f"{number_to_check} is a prime number.")
+else:
+    print(f"{number_to_check} is not a prime number.")
